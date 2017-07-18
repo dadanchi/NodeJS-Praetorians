971b0ebd221f3cf64985c6d72d396b7399cc528f
Yordan <dadanchi@abv.bg>
2017-07-18 12:27:26 +0300

    css and paths

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 /* eslint linebreak-style: ["error", "windows"]*/
+/* globals __dirname */
 const express = require('express');
+const path = require('path');
 const init = (data) => {
     const app = express();
 
     app.set('view engine', 'pug');
     require('./routers')
         .attachTo(app, data);
+
+    const libsPath = path.join(__dirname, '../node_modules/');
+    app.use('/libs', express.static(libsPath));
+
+    const staticsPath = path.join(__dirname, '../static');
+    app.use('/static', express.static(staticsPath));
 
     return Promise.resolve(app);
 };
