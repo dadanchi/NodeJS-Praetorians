d31573281431828471c532bd998a88c0b5f8323f
lazarlyutakov <lazarlyutakov@gmail.com>
2017-07-18 12:09:00 +0300

    setup mongodb

---
+++ config/config.js
@@ -0,0 +1,4 @@
+const port = 3001;
+const connectionString = 'mongodb://localhost';
+
+module.exports = { port, connectionString };
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 /* eslint linebreak-style: ["error", "windows"]*/
+/* eslint-disable no-console */
 
-const app = require('./app');
+const async = () => {
+    return Promise.resolve();
+};
 
-Promise.resolve(app.init())
-    .then((a) => a.listen(3001));
+const config = require('./config');
+
+async()
+    .then(() => require('./db').init(config.connectionString))
+    .then((app) => require('./app').init())
+    .then((app) => {
+        app.listen(config.port, () => console.log('Tva e server'));
+    });
