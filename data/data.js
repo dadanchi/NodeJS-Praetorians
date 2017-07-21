ad18649cbc7bccb2fb18e79aca13dbe7523ebe29
Martin Martinov <martinboykov@mail.bg>
2017-07-21 13:44:49 +0300

    forum items and nav

--- data/data.js
+++ data/data.js
@@ -1,10 +1,12 @@
 const UsersData = require('./users.data');
+const ItemsData = require('./items.data');
 
 const init = (db) => {
     return Promise.resolve({
         // topics
         // comments
         users: new UsersData(db),
+        items: new ItemsData(db),
     });
 };
 
