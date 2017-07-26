30c66ff5868fa60955f03471f58f1d1fc39cc6b8
lazarlyutakov <lazarlyutakov@gmail.com>
2017-07-26 17:41:55 +0300

    extend showPosts func

--- static/js/showPosts.js
+++ static/js/showPosts.js
@@ -1,7 +1,17 @@
 $(function () {
-    const btn = $('#show-posts-btn');
+    const showBtn = $('#posts-show-btn');
+    const hideBtn = $('#posts-hide-btn');
     const postUl = $('#posts-container');
-    btn.on('click', () => {
-        postUl.children().css('display', 'block');
-    });
+
+        showBtn.on('click', () => {
+            postUl.children().css('display', 'block');
+            showBtn.css('display', 'none');
+            hideBtn.css('display', 'block');
+        });
+
+        hideBtn.on('click', () => {
+            postUl.children('#user-posts-wrapper').css('display', 'none');
+            showBtn.css('display', 'block');
+            hideBtn.css('display', 'none');
+        });
 });
