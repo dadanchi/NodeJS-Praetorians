3d8c5eff4969cb3c72581d842cc550b2e4ec0d6a
Yordan <dadanchi@abv.bg>
2017-07-27 21:01:26 +0300

    user profile

--- static/js/showPosts.js
+++ static/js/showPosts.js
@@ -1,17 +1,17 @@
-$(function () {
+$(() => {
     const showBtn = $('#posts-show-btn');
     const hideBtn = $('#posts-hide-btn');
     const postUl = $('#posts-container');
 
-        showBtn.on('click', () => {
-            postUl.children().css('display', 'block');
-            showBtn.css('display', 'none');
-            hideBtn.css('display', 'block');
-        });
+    showBtn.on('click', () => {
+        postUl.children().css('display', 'block');
+        showBtn.css('display', 'none');
+        hideBtn.css('display', 'block');
+    });
 
-        hideBtn.on('click', () => {
-            postUl.children('#user-posts-wrapper').css('display', 'none');
-            showBtn.css('display', 'block');
-            hideBtn.css('display', 'none');
-        });
+    hideBtn.on('click', () => {
+        postUl.children('#user-posts-wrapper').css('display', 'none');
+        showBtn.css('display', 'block');
+        hideBtn.css('display', 'none');
+    });
 });
