be87767025891d7838fa505e194e3b7e2c421184
Yordan <dadanchi@abv.bg>
2017-07-27 13:44:05 +0300

    moved router stuff into controller (topics)

--- app/routers/topics.router/controller.js
+++ app/routers/topics.router/controller.js
@@ -1,17 +1,96 @@
+const helper = require('../../../helpers/helpers');
+
 const init = (data) => {
     const controller = {
         getAll(req, res) {
             let { page } = req.query;
             page = page || 1;
-            const size = 2;
+            const size = 8;
             return data.topics.getAllTopics()
                 .then((topics) => {
                     // catch error
-                        topics = topics.slice((page - 1) * size, page * size);
+                    const croppedPart = page * size;
+                    if (croppedPart > topics.length) {
+                        page = 1;
+                    }
+                    topics = topics.slice((page - 1) * size, page * size);
                     return res.render('topics/all', {
                         context: topics,
                     });
                 });
+        },
+
+        addTopic(req, res) {
+            const topic = req.body;
+            topic.author = req.user.username;
+            // validation server side
+            const comment = {
+                content: topic.content,
+                author: req.user.username,
+                date: helper.getDate(),
+            };
+            delete topic.content;
+            topic.comments = [];
+            topic.comments.push(comment);
+
+            return data.topics.create(topic)
+                .then((top) => {
+                    return res.redirect('/topics');
+                })
+                .catch((err) => {
+                    return res.redirect('/form');
+                });
+        },
+
+        searchTopic(req, res) {
+            const searh = req.query.searh;
+            return data.topics.findBy(searh)
+                .then((topics) => {
+                    return res.render('topics/allFiltered', {
+                        topics: topics,
+                    });
+                });
+        },
+
+        getCurrentTopic(req, res) {
+            const removedString = ':title=';
+            const title = req.params.title.substr(removedString.length);
+            const page = req.query.page || 1;
+            const size = 8;
+            return data.topics.findByTitle(title)
+                .then((topic) => {
+                    if (topic.comments.length !== 0) {
+                        // catch error
+                        topic.comments =
+                            topic.comments.slice((page-1) * size, page * size);
+                    }
+                    return res.render('topics/comments', {
+                        topic: topic,
+                    });
+                });
+        },
+
+        addComment(req, res) {
+            const removedString = ':title=';
+            const title = req.params.title.substr(removedString.length);
+            const comment = {
+                topic: title,
+                content: req.body.comment,
+                author: req.user.username,
+            };
+
+            return Promise.all([
+                data.users.addComment(comment),
+                data.topics.addComment(comment),
+            ])
+                .then(() => {
+                    return res.redirect(`/topics/:title=${title}`);
+                });
+            // for debugging
+            // .catch((err) => {
+            //     console.log(err);
+            //     return res.redirect(`/topics/:title=${title}`);
+            // });
         },
     };
 
--- app/routers/topics.router/router.js
+++ app/routers/topics.router/router.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const Topic = require('../../../models/topic.model');
-const helper = require('../../../helpers/helpers');
 
 const attachTo = (app, data) => {
     const controller = require('./controller').init(data);
@@ -11,35 +10,10 @@ const attachTo = (app, data) => {
             return controller.getAll(req, res);
         })
         .post('/', (req, res) => {
-            const topic = req.body;
-            topic.author = req.user.username;
-            // validation server side
-            const comment = {
-                content: topic.content,
-                author: req.user.username,
-                date: helper.getDate(),
-            };
-            delete topic.content;
-            topic.comments = [];
-            topic.comments.push(comment);
-
-            return data.topics.create(topic)
-                .then((top) => {
-                    return res.redirect('/topics');
-                })
-                .catch((err) => {
-                    return res.redirect('/form');
-                });
+            return controller.addTopic(req, res);
         })
-
         .get('/filtered', (req, res) => {
-            const searh = req.query.searh;
-            return data.topics.findBy(searh)
-                .then((topics) => {
-                    return res.render('topics/allFiltered', {
-                        topics: topics,
-                    });
-                });
+            return controller.searchTopic(req, res);
         })
         .get('/form', (req, res) => {
             if (req.user) {
@@ -48,50 +22,11 @@ const attachTo = (app, data) => {
             // expect todo --> popup message
             return res.redirect('/topics');
         })
-
-
         .get('/:title', (req, res) => {
-            const removedString = ':title=';
-            const title = req.params.title.substr(removedString.length);
-            const page = req.query.page || 1;
-            const size = 2;
-            return data.topics.findByTitle(title)
-                .then((topic) => {
-                    if (topic.comments.length !== 0) {
-                        // catch error
-                        topic.comments = topic.comments.slice((page-1) * size, page * size);
-                    }
-                    return res.render('topics/comments', {
-                        topic: topic,
-                    });
-                });
-            // get the comments about the topic here
+            return controller.getCurrentTopic(req, res);
         })
-        // .get('/:page', (req, res) => {
-        //     return controller.getPages(req, res);
-        // })
-        // TODO add a comment route
         .post('/:title/comments', (req, res) => {
-            const removedString = ':title=';
-            const title = req.params.title.substr(removedString.length);
-            const comment = {
-                topic: title,
-                content: req.body.comment,
-                author: req.user.username,
-            };
-
-            return Promise.all([
-                data.users.addComment(comment),
-                data.topics.addComment(comment),
-            ])
-                .then(() => {
-                    return res.redirect(`/topics/:title=${title}`);
-                });
-            // for debugging
-            // .catch((err) => {
-            //     console.log(err);
-            //     return res.redirect(`/topics/:title=${title}`);
-            // });
+            return controller.addComment(req, res);
         });
 
     app.use('/topics', router);
