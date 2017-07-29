603388c98e2d54aeac2244a8b403da2a9204b1a7
lazarlyutakov <lazarlyutakov@gmail.com>
2017-07-29 16:35:24 +0300

    up

--- helpers/validator.js
+++ helpers/validator.js
@@ -2,27 +2,28 @@ const notifier = require('node-notifier');
 const strToRemove = 'http://localhost:3001';
 
 const validatePassword = (req, res, password) => {
+    const redirectDirectory = req.headers.referer.substr(strToRemove.length);
     if (password === '' || password === null) {
         notifier.notify('Enter password');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (password.length < 6) {
         notifier.notify('Password must be at least 6 chars long');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (password.match(/[^a-zA-Z0-9 ]/)) {
         notifier.notify('Password must contain only letters a-zA-Z and digits 0-9');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (password.includes(' ')) {
         notifier.notify('Password must not contain white spaces');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
@@ -30,27 +31,28 @@ const validatePassword = (req, res, password) => {
 };
 
 const validateUsername = (req, res, username) => {
+    const redirectDirectory = req.headers.referer.substr(strToRemove.length);
     if (username === '' || username === null) {
         notifier.notify('Enter username');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (username.length < 3 || typeof username !== 'string') {
         notifier.notify('Username must be at least 3 chars long string');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (username.match(/[^a-zA-Z0-9 ]/)) {
         notifier.notify('Username must contain only letters a-zA-Z and digits 0-9');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
 
     if (username.includes(' ')) {
         notifier.notify('Username must not contain white spaces');
-        res.redirect('/auth/sign-up');
+        res.redirect(redirectDirectory);
         return false;
     }
     return true;
