c7afc8851305d6a8268b50e1f7c2d532469c8f0e
Martin Martinov <martinboykov@mail.bg>
2017-07-31 01:03:53 +0300

    add comments.data.tests

--- data/comments.data.js
+++ data/comments.data.js
@@ -17,27 +17,6 @@ class CommentsData extends BaseData {
             _id: new ObjectID(id),
         });
     }
-    modify(id, newContent) {
-        return this.collection.findOne({
-            _id: new ObjectID(id),
-        })
-            .then((comment) => {
-                // comment.content = newContent;
-                // console.log(comment);
-                return this.collection.updateOne(
-                    { 'topic': `${comment.topic}` },
-                    { 'author': `${comment.author}` },
-                    { 'authorId': `${comment.authorId}` },
-                    { 'date': `${comment.date}` },
-                    { '_id': `${comment._id}` },
-                    {
-                        $set: {
-                            'content': `${newContent}`,
-                        },
-                    }
-                );
-            });
-    }
 
     _isModelValid(model) {
         // custom validation
