8644f28a940022727b835adc6f07179ca24693f3
Yordan <dadanchi@abv.bg>
2017-08-01 01:34:11 +0300

    topics controller tests

--- tests/unit/app/routers/topics.router/controller/addComment.js
+++ tests/unit/app/routers/topics.router/controller/addComment.js
@@ -3,16 +3,22 @@ const { expect } = require('chai');
 const { init } =
     require('../../../../../../app/routers/topics.router/controller');
 
-describe('controller.addTopic', () => {
+describe('controller.addComment', () => {
     let controller = null;
-    const topic = { 1: 1 };
+    const topic = {
+        _id: 2,
+        title: 'evalarka',
+    };
     let res = null;
     let req = null;
     const comment = 'dsa';
     const data = {
         topics: {
-            create() {
+            findByTitle() {
                 return Promise.resolve(topic);
+            },
+            addComment() {
+
             },
         },
         comments: {
@@ -28,16 +34,16 @@ describe('controller.addTopic', () => {
     };
 
     const options = {
-        body: {
-            topic: {
-                title: 'dsa',
-                content: 'dsa',
+            params: {
+                title: 'gosho',
             },
-        },
-        user: {
-            username: 'gosho',
-            _id: 1,
-        },
+            body: {
+                comment: comment,
+            },
+            user: {
+                username: 'pesho',
+                _id: 1,
+            },
     };
 
     controller = init(data);
@@ -45,9 +51,10 @@ describe('controller.addTopic', () => {
     res = require('../../../../req-res').getResponseMock();
     req = require('../../../../req-res').getRequestMock(options);
     it('expect to redirect to /topics', () => {
-        return controller.addTopic(req, res)
+        return controller.addComment(req, res)
             .then(() => {
-                expect(res.redirectUrl).to.be.deep.equal('/topics');
+                expect(res.redirectUrl).to.be.deep.equal(
+                    `/topics/${options.params.title}`);
             });
     });
 });
