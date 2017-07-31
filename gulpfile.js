6314fa650f36b03be1488d602b61dbd2bebda888
Martin Martinov <martinboykov@mail.bg>
2017-08-01 01:59:09 +0300

    integration tests

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('pre-test', () => {
 gulp.task('tests:unit', ['pre-test'], () => {
     return gulp.src([
         './tests/unit/**/*.js',
-        './test/integration/**/*.js',
+        './tests/integration/**/*.js',
     ])
         .pipe(mocha({
             reporter: 'nyan',
@@ -36,31 +36,31 @@ gulp.task('tests:unit', ['pre-test'], () => {
         .pipe(istanbul.writeReports());
 });
 
-const config = {
-    connectionString: 'mongodb://localhost/forum-db-test',
-    port: 3002,
-};
+// const config = {
+//     connectionString: 'mongodb://localhost/forum-db-test',
+//     port: 3002,
+// };
 
-gulp.task('test-server:start', () => {
-    return Promise.resolve()
-        .then(() => require('./db').init(config.connectionString))
-        .then((db) => require('./data').init(db))
-        .then((data) => require('./app').init(data))
-        .then((app) => {
-            app.listen(
-                config.port,
-                () => console.log(`Magic happends at :${config.port}`));
-        });
-});
+// gulp.task('test-server:start', () => {
+//     return Promise.resolve()
+//         .then(() => require('./db').init(config.connectionString))
+//         .then((db) => require('./data').init(db))
+//         .then((data) => require('./app').init(data))
+//         .then((app) => {
+//             app.listen(
+//                 config.port,
+//                 () => console.log(`Magic happends at :${config.port}`));
+//         });
+// });
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-gulp.task('test-server:stop', () => {
-    return MongoClient.connect(config.connectionString)
-        .then((db) => {
-            return db.dropDatabase();
-        });
-});
+// gulp.task('test-server:stop', () => {
+//     return MongoClient.connect(config.connectionString)
+//         .then((db) => {
+//             return db.dropDatabase();
+//         });
+// });
 
 // gulp.task('tests:browser', ['test-server:start'], () => {
 //     return gulp.src('./test/browser/items/create-item.js')
---
+++ tests/integration/topics.tests.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+const { init } = require('../../app/');
+describe('/topics tests', () => {
+    const connectionString = 'mongodb://localhost/forum-db-test';
+    let app = null;
+
+    beforeEach(() => {
+        return Promise.resolve()
+            .then(() => require('../../db').init(connectionString))
+            .then((db) => require('../../data').init(db))
+            .then((data) => require('../../app').init(data))
+            .then((_app) => {
+                app = _app;
+            });
+    });
+
+    describe('GET /topics/form', () => {
+
+        it('expect to return 302', (done) => {
+            request(app)
+                .get('/topics/form')
+                .expect(302)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    return done();
+                });
+        });
+    });
+     describe('GET /topics/filtered', () => {
+          const controller = {
+            searchTopic() {
+                return done();
+                },
+          }
+        it('expect to return 200', (done) => {
+            request(app)
+                .get('/topics/filtered')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    return done();
+                });
+        });
+    });
+});
