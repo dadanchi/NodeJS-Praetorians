c28c4d1f9ff039674ae4d41d2ad9ce6d9a2e34a7
martinboykov <martinboykov>
2017-07-31 23:57:21 +0000

    .

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-const port = 3001;
-const connectionString = 'mongodb://localhost/forum-db';
+const port = 80;
+const connectionString = 'mongodb://35.158.250.200:27017/forum-db';
 const sessionSecret = 'Gosho 4ever';
 
 module.exports = { port, connectionString, sessionSecret };
